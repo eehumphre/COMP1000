f26c9c624e90d7f57f4961813084add4a66cf121
Eleanor Humphreys <humphre@simmons.edu>
2016-04-12 09:33:33 -0400

    Lab two!

--- workspace/Submissions/src/Main.java
+++ workspace/Submissions/src/Main.java
@@ -7,30 +7,37 @@ public class Main {
 		System.out.print("Noun: ");
 		noun = scanner.nextLine();
 		//System.out.println();
-
+		System.out.println();
 		System.out.print("Your new pet is a ");
 		System.out.print(noun);
 		
 		String adjective;
         adjective = scanner.nextLine(); 
+        System.out.println();
+        
         System.out.print("adjective: "); 
         adjective = scanner.nextLine();  
+        System.out.println();
         System.out.print("It is a very ");
+        
         System.out.print(adjective);
         System.out.print(" pet.");
+        System.out.println();
         String verb;
         verb = scanner.nextLine();
         System.out.print("Verb:");
+        System.out.println();
         verb = scanner.nextLine();
         System.out.print("Your pet is ");
         System.out.print(verb ); 
         System.out.print(" away from you! You better go catch it!");
-        System.out.print("You");
+        System.out.println();
        
         int number;
         long start = System.nanoTime();
         noun = scanner.nextLine(); 
         System.out.println("Enter an age for your pet: ");
+        
         number = scanner.nextShort(); 
        
         if (number < 100) {
